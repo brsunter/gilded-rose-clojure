2ef7aa9e4eedbf52269a7dd42d74ed395c978a11
Brian Sunter <bsunter@gmail.com>
2017-04-28 22:03:48 -0700

    Fix Spec

--- src/gilded_rose/items/backstage_pass.clj
+++ src/gilded_rose/items/backstage_pass.clj
@@ -6,17 +6,17 @@
 
 (s/def ::backstage-pass-quality-level
   (s/or :over #(> 0 %)
-        :normal #(> 10 %)
-        :rising #(>= 6 % 10)
-        :peak #(>= 0 % 5)))
+        :normal #(< 10 %)
+        :rising #(<= 6 % 10)
+        :peak #(<= 0 % 5)))
 
 (defn update-backstage-pass-quality
   [pass quality-level]
   (case quality-level
     :over (assoc pass :quality 0)
-    :normal (update pass :quality inc)
-    :rising (update pass :quality #(+ 2 %))
-    :peak (update pass :quality #(+ 3 %))))
+    :normal (i/increase-quality pass 1)
+    :rising (i/increase-quality pass 2)
+    :peak (i/increase-quality pass 3)))
 
 (defmethod i/update-item backstage-pass-name
   [{:keys [sell-in] :as item}]
--- src/gilded_rose/items/item.clj
+++ src/gilded_rose/items/item.clj
@@ -2,6 +2,15 @@
 
 (defmulti update-item :name)
 
+(def max-quality 50)
+
 (defn decrease-quality
   [item x]
   (update item :quality #(Math/abs (- % x))))
+
+(defn increase-quality
+  [{:keys [quality] :as item} x]
+  (let [next-quality (+ quality x)]
+    (if (<= next-quality max-quality)
+      (assoc item :quality next-quality)
+      (assoc item :quality max-quality))))
